2408b63afa5378f48a09f0e0cd6afd28998d4589
rshekar005 <rshekar005@gmail.com>
2019-10-10 18:20:07 +0530

    Added Sorting of characters based on input -- Interview question

---
+++ src/Interview/SortCharacters.java
@@ -0,0 +1,37 @@
+package Interview;
+
+public class SortCharacters 
+{
+	
+	public static void main(String args[])
+	{
+		String input="AbBCdcaD";
+		char[] charArray= input.toCharArray();
+		System.out.println(charArray);
+		String output="";
+		for(int i=0;i<charArray.length;i++)
+		{
+			int ASCII= charArray[i];
+			if(ASCII<97)
+			{
+				output=output+charArray[i];
+			}
+			
+			for(int j=0;j<charArray.length;j++)
+			{
+				if(i!=j)
+				{
+					int ASCIInew= charArray[j];
+					if(ASCII+32==ASCIInew)
+					{
+						output=output+charArray[j];
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(output);
+	}
+	
+
+}
