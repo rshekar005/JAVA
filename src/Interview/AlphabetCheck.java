696315b3d9fb5a47bf10f95f5ae3ce22e792fc25
rshekar005 <rshekar005@gmail.com>
2020-07-16 09:15:03 +0530

    Updated by printing ascill value

--- src/Interview/AlphabetCheck.java
+++ src/Interview/AlphabetCheck.java
@@ -4,6 +4,8 @@ public class AlphabetCheck {
 
 	public static void main(String[] args) {
 		char c='$';
+		int a=c;
+		System.out.println(a);// It will print ascii value
 		if((c>='a' && c<='z') || (c>='A' && c<='z'))
 		{
 			System.out.println(c+ " is an alphabet");
