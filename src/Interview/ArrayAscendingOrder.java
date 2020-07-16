07103e2c691398157811cdbd8a5ff8c097d71a55
rshekar005 <rshekar005@gmail.com>
2020-07-16 14:34:37 +0530

    Modified code

--- src/Interview/ArrayAscendingOrder.java
+++ src/Interview/ArrayAscendingOrder.java
@@ -16,7 +16,7 @@ public class ArrayAscendingOrder {
         //Sort the array in ascending order    
         for (int i = 0; i < arr.length; i++) {     
             for (int j = i+1; j < arr.length; j++) {     
-               if(arr[i] > arr[j]) {    
+            	if(arr[i] > arr[j]) {    
                    temp = arr[i];    
                    arr[i] = arr[j];    
                    arr[j] = temp;    
