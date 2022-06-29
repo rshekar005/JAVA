806c44d86785b1d706851202edeb00e304c0e3ae
rshekar005 <rshekar005@gmail.com>
2022-06-29 14:46:31 +0530

    Added new files

---
+++ src/Interview/StringM.java
@@ -0,0 +1,13 @@
+package Interview;
+
+public class StringM {
+    public static void main(String[] args) {
+        String str="AEIUBCDM";
+        //output: IEACBUDM
+        String newStr = "";
+        char[] ch=str.toCharArray();
+
+        System.out.println(newStr);
+
+    }
+}
