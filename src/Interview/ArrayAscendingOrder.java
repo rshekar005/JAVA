73622b42c32e825955fa3633d2403f39fa8bbc1a
rshekar005 <rshekar005@gmail.com>
2022-06-30 12:43:14 +0530

    ArrayAscendingOrder.java

--- src/Interview/ArrayAscendingOrder.java
+++ src/Interview/ArrayAscendingOrder.java
@@ -1,12 +1,14 @@
 package Interview;
 
+import java.util.Arrays;
+
 public class ArrayAscendingOrder {
 
 	public static void main(String[] args) {
 		 //Initialize array     
         int [] arr = new int [] {5, 2, 8, 7, 1};     
         int temp = 0;    
-            
+
         //Displaying elements of original array    
         System.out.println("Elements of original array: ");    
         for (int i = 0; i < arr.length; i++) {     
