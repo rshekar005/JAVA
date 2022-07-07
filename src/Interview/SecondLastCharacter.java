7baea14f8ec045afc89388deaacf7133513f58a8
rshekar005 <rshekar005@gmail.com>
2022-07-07 17:57:43 +0530

    Added code to find longest sub string

--- src/Interview/SecondLastCharacter.java
+++ src/Interview/SecondLastCharacter.java
@@ -5,8 +5,6 @@ public class SecondLastCharacter {
         String str="Rajashekar";
         System.out.println("Length of the String is "+str.length());
         int i=str.length()-2;
-        System.out.println(i);
-        char c[]=str.toCharArray();
-        System.out.println(c[i]);
+        System.out.println(str.charAt(i));
     }
 }
